abc8457ad0f97ad8fdb74a3576ee847e9b239043
Aaron Mell <aaronamell@gmail.com>
2025-10-21 14:20:10 -0400

    fix: adding missing bin folder

---
+++ bin/dev.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning --disable-warning=DeprecationWarning
+
+// eslint-disable-next-line n/shebang
+import { execute } from '@oclif/core';
+
+await execute({ development: true, dir: import.meta.url });
