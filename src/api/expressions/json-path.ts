2b7612760d1a636b63dc298dd733605b22561986
Aaron Mell <aaronamell@gmail.com>
2026-01-07 09:10:53 -0500

    feat: added abilty to specify several built in json objects to jsonPath

--- src/api/expressions/json-path.ts
+++ src/api/expressions/json-path.ts
@@ -1,6 +1,47 @@
 import { ExpressionArgs, getVariableReference } from '../expression.js';
 import { JsonPathExpression } from './classes.js';
 
-export function jsonPath(input: ExpressionArgs, value: string): JsonPathExpression {
-    return new JsonPathExpression(`jsonpath(${getVariableReference(input)}, '${value}')`);
+export class WorkflowParametersJson {
+    isWorkflowParametersJson = true;
+}
+
+export class WorkflowAnnotationsJson {
+    isWorkflowAnnotationsJson = true;
+}
+
+export class WorkflowLabelsJson {
+    isWorkflowLabelsJson = true;
+}
+
+export class CronWorkflowAnnotationsJson {
+    isCronWorkflowAnnotationsJson = true;
+}
+
+export class CronWorkflowLabelsJson {
+    isCronWorkflowLabelsJson = true;
+}
+
+export function jsonPath(
+    input:
+        | ExpressionArgs
+        | WorkflowParametersJson
+        | WorkflowAnnotationsJson
+        | WorkflowLabelsJson
+        | CronWorkflowAnnotationsJson
+        | CronWorkflowLabelsJson,
+    value: string,
+): JsonPathExpression {
+    if ((input as WorkflowParametersJson)?.isWorkflowParametersJson) {
+        return new JsonPathExpression(`jsonpath(workflow.parameters.json, '${value}')`);
+    } else if ((input as WorkflowAnnotationsJson)?.isWorkflowAnnotationsJson) {
+        return new JsonPathExpression(`jsonpath(workflow.annotations.json, '${value}')`);
+    } else if ((input as WorkflowLabelsJson)?.isWorkflowLabelsJson) {
+        return new JsonPathExpression(`jsonpath(workflow.labels.json, '${value}')`);
+    } else if ((input as CronWorkflowAnnotationsJson)?.isCronWorkflowAnnotationsJson) {
+        return new JsonPathExpression(`jsonpath(cronworkflow.annotations.json, '${value}')`);
+    } else if ((input as CronWorkflowLabelsJson)?.isCronWorkflowLabelsJson) {
+        return new JsonPathExpression(`jsonpath(cronworkflow.labels.json, '${value}')`);
+    } else {
+        return new JsonPathExpression(`jsonpath(${getVariableReference(input as ExpressionArgs)}, '${value}')`);
+    }
 }
