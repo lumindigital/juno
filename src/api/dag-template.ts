0edf08f29d3d8dad9bd8cabd5169911c07772637
Aaron Mell <aaronamell@gmail.com>
2026-01-09 15:08:10 -0500

    feat: the DagTemplate has a new parameter named targets
    targets accepts ExpressionArgs[] | UndefinedExpressionArg[]

--- src/api/dag-template.ts
+++ src/api/dag-template.ts
@@ -1,20 +1,48 @@
 import { IoArgoprojWorkflowV1Alpha1DAGTemplate } from '../workflow-interfaces/data-contracts.js';
 import { DagTask } from './dag-task.js';
+import { ExpressionArgs } from './expressions/types.js';
+import { simpleTag, UndefinedExpressionArg } from './expressions/tag.js';
 
 export class DagTemplate {
     failFast?: boolean;
     target?: string;
+    targets?: ExpressionArgs[] | UndefinedExpressionArg[];
     tasks!: DagTask[];
 
     constructor(init: Partial<DagTemplate>) {
         Object.assign(this, init);
     }
 
-    toDagTemplate(): IoArgoprojWorkflowV1Alpha1DAGTemplate {
+    toDagTemplate(templateName: string): IoArgoprojWorkflowV1Alpha1DAGTemplate {
+        this.validateMutuallyExclusive(templateName);
+
+        let target: string | undefined = undefined;
+
+        if (this.target) {
+            target = this.target;
+        } else if (this.targets && this.targets.length > 0) {
+            target = this.targets.map((t) => simpleTag(t)).join(', ');
+        }
+
         return {
             failFast: this.failFast,
-            target: this.target,
+            target: target,
             tasks: this.tasks.map((x) => x.toDagTask()),
         };
+    }
+
+    validateMutuallyExclusive(templateName: string): void {
+        let count = 0;
+        if (this.target) {
+            count++;
+        }
+
+        if (this.targets && this.targets.length > 0) {
+            count++;
+        }
+
+        if (count > 1) {
+            throw new Error(`target and targets are mutually exclusive in DagTemplate for dag named ${templateName}`);
+        }
     }
 }
